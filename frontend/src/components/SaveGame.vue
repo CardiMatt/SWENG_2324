24ea5c43f52a342a2df40991caa6e92790a13810
MichelaPagliarani <michela.pagliarani4@studio.unibo.it>
2025-01-09 22:43:20 +0100

    prova

---
+++ frontend/src/components/SaveGame.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="save-game">
+      <h2>Salva Partita</h2>
+      <button @click="saveProgress">Salva</button>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  import { GameSaveRepository } from '@/repositories/GameSaveRepository';
+  import type { GameSave } from '@/models/GameSave';
+  import { v4 as uuidv4 } from 'uuid';
+  //TODO scegliere se usare uuid (generatore id univoci di javascript o funzione firebase add(). vantaggio uuid gestire tutto localmente)
+  
+  export default defineComponent({
+    name: 'SaveGame',
+    props: {
+      userId: {
+        type: String,
+        required: true,
+      },
+      storyId: {
+        type: String,
+        required: true,
+      },
+      progress: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      inventory: {
+        type: String,
+        required: true,
+      },
+    },
+    methods: {
+      async saveProgress() {
+        const gameSave: GameSave = {
+            id: uuidv4(),
+            userId: this.userId,
+            storyId: this.storyId,
+            progress: this.progress,
+            saveDate: new Date(),
+            state: this.state,
+            inventory: this.inventory
+        };
+  
+        await GameSaveRepository.saveGameSave(gameSave);
+        alert('Salvataggio completato!');
+      },
+    },
+  });
+  </script>
+  
+  <style scoped>
+  .save-game {
+    margin: 20px;
+  }
+  button {
+    background-color: #2ecc71;
+    color: white;
+    border: none;
+    padding: 10px;
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: #27ae60;
+  }
+  </style>
+  
